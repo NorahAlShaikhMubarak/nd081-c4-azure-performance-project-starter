2a938940bc027562ca91583f3e7737ad1b088dbf
NorahAlShaikhMubarak <norahhamad98@outlook.com>
2023-01-03 21:35:09 +0300

    final commit

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -63,22 +63,11 @@ else:
 if ("TITLE" in os.environ and os.environ['TITLE']):
     title = os.environ['TITLE']
 else:
-    title = app.config['TITLE'] 
+    title = app.config['TITLE']
 
 # Redis Connection
-# r = redis.Redis()  # for VM Scale set deployment
-redis_server = os.environ['REDIS']
-try:
-    if "REDIS_PWD" in os.environ:
-        r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
-    else:
-        r = redis.Redis(redis_server)
-    r.ping()
-except redis.ConnectionError:
-    exit('Failed to connect to Redis, terminating.')
- 
+r = redis.Redis()
+
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
